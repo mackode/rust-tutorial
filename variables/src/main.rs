8d8e72d408dc487c81f67d407a59e94abbce59a4
Bartłomiej Makowski <biuro@mackode.pl>
2017-11-26 13:54:38 +0100

    Variables: shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
 }
