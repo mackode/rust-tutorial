b519a235b7886419eb89fa6d80f6bc1f13c9f7c3
Bartłomiej Makowski <biuro@mackode.pl>
2018-08-01 07:03:42 +0200

    Comments

---
+++ comments/src/main.rs
@@ -0,0 +1,7 @@
+
+// So we’re doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully, this comment will
+// explain what’s going on.
+fn main() {
+    let lucky_number = 7; // I’m feeling lucky today
+}
