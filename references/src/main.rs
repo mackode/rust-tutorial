fe98d3bd7e56eae9e452ecb6652b07776d819820
Bartłomiej Makowski <biuro@mackode.pl>
2018-08-05 13:27:46 +0200

    References and borrowing

---
+++ references/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+    change(&mut s);
+
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r2 = &mut s;
+
+    let reference_to_nothing = no_dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+/*
+fn dangle() -> &String { // dangle returns a reference to a String
+    let s = String::from("hello"); // s is a new String
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
+*/
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
+}
