65ba885b4ed5058a7f27e793ee03eb4c89411be8
Bartłomiej Makowski <biuro@mackode.pl>
2018-08-12 09:18:43 +0200

    Methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,20 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -26,6 +40,19 @@ fn main() {
 
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
 }
 
 fn area0(width: u32, height: u32) -> u32 {
