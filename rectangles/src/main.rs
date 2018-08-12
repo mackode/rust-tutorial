33591444f1fab732d02409c6454b5c7380b1a9ec
Bartłomiej Makowski <biuro@mackode.pl>
2018-08-12 09:09:31 +0200

    Structs example; rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area0(width1, height1)
+    );
+
+    let rect1 = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area1(rect1)
+    );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area2(&rect1)
+    );
+
+    println!("rect1 is {:?}", rect1);
+    println!("rect1 is {:#?}", rect1);
+}
+
+fn area0(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area1(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area2(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
