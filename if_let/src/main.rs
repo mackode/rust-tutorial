220545f54d732d4818d0bc08259e8e73d5260c50
Bartłomiej Makowski <biuro@mackode.pl>
2018-08-16 10:36:14 +0200

    If let

---
+++ if_let/src/main.rs
@@ -0,0 +1,13 @@
+
+fn main() {
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+}
