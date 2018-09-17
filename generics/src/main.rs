1ab585a22c1a7b9def3d9f37bd3381498295ba09
Bartłomiej Makowski <biuro@mackode.pl>
2018-09-17 23:18:06 +0200

    Generics

---
+++ generics/src/main.rs
@@ -0,0 +1,85 @@
+/*
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+*/
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for ref item in list.iter() {
+        if item > &largest {
+            largest = &item;
+        }
+    }
+
+    largest
+}
+
+/*
+struct Point<T> {
+    x: T,
+    y: T,
+}
+*/
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+/*
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+*/
+
+/*
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+*/
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+
+    //let p = Point { x: 5, y: 10 };
+    //println!("p.x = {}", p.x());
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
