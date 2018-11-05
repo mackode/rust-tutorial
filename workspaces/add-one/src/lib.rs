7b9518c10671610527e44a7c173ddef5ccff456a
Bartłomiej Makowski <biuro@mackode.pl>
2018-11-05 10:14:08 +0100

    Workspaces

---
+++ workspaces/add-one/src/lib.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
+}
