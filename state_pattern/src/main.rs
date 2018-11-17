10a3543cea95088eac649996c42c36319a325d43
Bartłomiej Makowski <biuro@mackode.pl>
2018-11-17 20:56:48 +0100

    If let pattern

---
+++ state_pattern/src/main.rs
@@ -0,0 +1,27 @@
+mod lib;
+use lib::Post;
+
+fn main() {
+    /*
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+    */
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
